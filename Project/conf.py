d30f4f5f40e4a6768f2d1edd0404e03e21bdde77
Weifeng Jin <wjin@uw.edu>
2017-08-30 14:53:06 +0800

    Create conf.py

---
+++ Project/conf.py
@@ -0,0 +1,17 @@
+# A configuration file for the Twisted Places proxy herd
+
+# Google Places API key
+API_KEY="AIzaSyBKPp6GE9dX4_VUbUWB7haqIfn8yxz2ReQ"
+
+# TCP port numbers for each server instance (server ID: case sensitive)
+# Please use the port numbers allocated by the TA.
+PORT_NUM = {
+	"Alford" : 12640,
+	"Hamilton" : 12641,
+	"Welsh" : 12642,
+	"Ball" : 12643,
+	"Holiday" : 12644 
+}
+
+
+PROJ_TAG="Fall 2016"
